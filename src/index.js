7e986aa689b230e0131a9c132f733dfc7775128f
LyssenkoAlex <lyssenko.alex@gmail.com>
2020-03-02 14:45:53 +0600

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  return Number(Math.abs(nums).toString().split('').reverse().join(''));
+  return Number(Math.abs(n).toString().split('').reverse().join(''));
 }
